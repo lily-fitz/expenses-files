752af23ecba8050635532cb00cb95f133b43a522
Lily <lily@444s-Air.attlocal.net>
2023-07-12 02:36:20 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const expenseForm = document.getElementById('expense-form')
+const itemInput = document.getElementById('item-input')
+const costInput = document.getElementById('cost-input')
+const expenseList = document.getElementById('expense-list')
+
+// Create an empty array for our expenses
+
+// When someone clicks submit:
+// Stop page from refreshing
+
+// Grab the value of the inputs
+
+// Validate Item Input: Field is empty? Fails => Don't submit form
+
+// Validate Cost Input: Field is empty? Fails => Don't submit form
+
+// Turn the input string into a number
+
+// Validate Cost Input: Value is a number? Fails => Don't submit form
+
+// Passes => Create new expense object with item and cost
+
+// Add new expense object to expenses array
+
+// Clear inputs
+
+// Display expenses as a list item
+
+// Last: Check to see if the form was submitted
